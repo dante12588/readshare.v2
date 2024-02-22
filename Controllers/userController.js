903081506a5bfba1b181a455409d38fdc5518716
Przemek Ćwiertka <przemek.cwiertka@gmail.com>
2024-02-22 17:39:40 +0100

    feat(flash): config flash

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,16 +1,46 @@
 const router = require('express').Router();
 // const User = require('../Models/User');
 // const Book = require('../Models/Book');
-const Models = require('../Models/Models');
+// const Models = require('../Models/Models');
 
 // Book.belongsTo(User, {foreignKey: 'userId'});
 // User.hasMany(Book, {foreignKey: 'userId'});
 
 // User.createUser('ni2ck', 'ma2il', 'ha2slo');
 
-router.get('/user', (req, res) => {
-   
-    res.send('User');
+
+// register new user
+router.post('/users/register', (req, res) => {
+    req.session.messages = {
+        type: 'success',
+        intro: 'Thank you!',
+        message: 'You have now'
+    };
+    res.redirect(303, '/register');
+});
+
+router.get('/register', (req, res) => {
+    res.render('register');
+});
+
+// login user
+router.post('/users/login', (req, res) => {
+    res.send('login');
+});
+
+// get user by id
+router.get('/users/:userid', (req, res) => {
+    res.send('get user by id');
+});
+
+// update user by id
+router.put('/users/:userid', (req, res) => {
+    res.send('update user by id');
+});
+
+// delete user by id
+router.delete('/users/:userid', (req, res) => {
+    res.send('delete user by id');
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -2,7 +2,18 @@ const express = require('express');
 const hbs  = require('express-handlebars');
 const hand = require('./lib/handlers');
 const morgan = require('morgan');
-const Controllers = require('./Controllers/index');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+// const flash = require('connect-flash');
+
+// configure flash middleware
+// app.use(flash());
+
+// // Ustawienie lokalnych zmiennych dla wiadomości flash
+// app.use((req, res, next) => {
+//     res.locals.messages = req.flash();
+//     next();
+//   });
 
 // const winston = require('winston');
 
@@ -29,6 +40,24 @@ const port = 4000;
 
 app.use(morgan('dev'));
 
+app.use(cookieParser());
+
+// session middleware
+app.use(session({
+    secret: 'jakistamteskt',
+    resave: false,
+    saveUninitialized: false
+  }));
+
+  // configure flash middleware
+// app.use(flash());
+
+// Ustawienie lokalnych zmiennych dla wiadomości flash
+
+
+const Controllers = require('./Controllers/index');
+
+
 // Set handlebars as the template engine
 // hbs({defaultLayout: 'main'}) not working
 app.engine('handlebars', hbs.engine({
@@ -41,8 +70,15 @@ app.get('/', hand.home);
 app.get('/about', hand.about);
 
 
-app.use('/', Controllers);
 
+
+app.use((req, res, next) => {
+  res.locals.messages = req.session.messages;
+  delete req.session.messages;
+  next();
+});
+
+app.use('/', Controllers);
 
 // app.listen(port, () => {
 //     console.log(`Example app listening at http://localhost:${port}`);
